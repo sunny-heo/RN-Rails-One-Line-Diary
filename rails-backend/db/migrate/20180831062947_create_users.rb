82496c51d72deb8b3b1ad92d54089703d8b65579
Sunny Heo <mobileheo@gmail.com>
2018-08-30 23:35:27 -0700

    Add gender column on user table and validation

--- rails-backend/db/migrate/20180831062947_create_users.rb
+++ rails-backend/db/migrate/20180831062947_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, unique: true
       t.string :password_digest
       t.string :first_name
       t.string :last_name
-      t.boolean :admin
+      t.string :gender
+      t.boolean :admin, null: false, default: false
 
       t.timestamps
     end
