e628dbe66f5ce77241b086970ff32e94530a0704
Sunny Heo <mobileheo@gmail.com>
2018-09-03 19:11:58 -0700

    Add routes for Diary and DailyContent

--- rails-backend/config/routes.rb
+++ rails-backend/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :session, only: [ :create, :destroy ]
       resources :users, only: [:create]
+      resources :diaries, shallow:true do 
+        resources :daily_contents
+      end
     end
   end
 end
