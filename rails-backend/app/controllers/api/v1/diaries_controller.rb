572447066a8f4c2914698dd8e4175ec463ee58db
Sunny Heo <mobileheo@gmail.com>
2018-09-03 20:35:33 -0700

    Add update action on Diary controller

--- rails-backend/app/controllers/api/v1/diaries_controller.rb
+++ rails-backend/app/controllers/api/v1/diaries_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::DiariesController < Api::ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  before_action :find_diary, only: [:update, :destroy]
+
   def index
     diaries = Diary.where(:user => current_user).order(created_at: :desc)
     render json: diaries
@@ -14,17 +16,27 @@ class Api::V1::DiariesController < Api::ApplicationController
     else 
       render json: { error: @diary.errors }
     end
+  end
 
+  def update
+    if @diary.update diary_params
+      render json: { message: "'#{@diary.name}' has been updated" }
+    else
+      render json: { error: @diary.errors }
+    end
   end
 
   def destroy
-    diary = Diary.find params[:id]
-    diary.destroy
+    @diary.destroy
     render json: { message: "'#{diary.name}' has been removed" }
   end
 
+
   private
 
+  def find_diary
+    @diary = Diary.find params[:id]
+  end
   
   def diary_params
     params.require(:diary).permit(:name, :disclose_date)
