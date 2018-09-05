a1a33212848ec677144a10f02bc8c9892a9691bf
Sunny Heo <mobileheo@gmail.com>
2018-09-04 23:49:57 -0700

    Add diary create action that sends post request

--- react_native_client/src/_requests/diaryService.js
+++ react_native_client/src/_requests/diaryService.js
@@ -3,7 +3,15 @@ import service from "./AxiosService";
 const DIARIES = "diaries";
 
 export default {
-  async diaryIndex() {
+  async index() {
+    try {
+      const diaries = await service.get(`/${DIARIES}`);
+      return diaries;
+    } catch (error) {
+      console.log("Error in diaryAll request => ", error);
+    }
+  },
+  async create() {
     try {
       const diaries = await service.get(`/${DIARIES}`);
       return diaries;
