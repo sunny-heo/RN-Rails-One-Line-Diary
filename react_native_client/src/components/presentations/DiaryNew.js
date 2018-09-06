961c2ae4e87d39d9534c5216c71e95b9cb8449b6
Sunny Heo <mobileheo@gmail.com>
2018-09-05 18:29:50 -0700

    Add basic validation for inputs to create new diary

--- react_native_client/src/components/presentations/DiaryNew.js
+++ react_native_client/src/components/presentations/DiaryNew.js
@@ -28,8 +28,9 @@ class DiaryNew extends Component {
     this.state = {
       name: "",
       isDateTimePickerVisible: false,
-      discloseDate: "",
-      selectedItems: []
+      discloseDate: null,
+      selectedItems: [],
+      errors: {}
     };
 
     this.nameRef = this._updateRef.bind(this, "name");
@@ -76,8 +77,7 @@ class DiaryNew extends Component {
   }
 
   componentDidMount() {
-    // console.log("componentDidMount()");
-    // this.name.focus();
+    this.name.focus();
   }
 
   _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
@@ -90,7 +90,6 @@ class DiaryNew extends Component {
   };
 
   _onChangeText = text => {
-    console.log();
     ["name"]
       .map(name => ({ name, ref: this[name] }))
       .forEach(({ name, ref }) => {
@@ -112,21 +111,47 @@ class DiaryNew extends Component {
     this.setState({ selectedItems });
   };
 
-  _onSubmit = async () => {
-    // console.log(this.props);
+  _validateInputs = () => {
+    let errors = {};
     const { name, discloseDate } = this.state;
-    const disclose_date = discloseDate.toString();
+    const inputObj = { name, discloseDate };
 
-    await this.props.dispatch(diaryActions.create({ name, disclose_date }));
-    const { navigation, diary } = this.props;
-    navigation.navigate(diary.fulfilledCreate ? "TodayIndex" : "DiaryNew");
+    Object.keys(inputObj).forEach(input => {
+      const value = this.state[input];
+      if (!value) {
+        errors[input] = "Should not be empty";
+      } else if (input === "discloseDate" && discloseDate <= new Date()) {
+        errors[input] = `Disclose date should be later than ${format(
+          new Date(),
+          "MMM Do, YYYY"
+        )}`;
+      }
+    });
+
+    this.setState({ errors });
+  };
+
+  _onSubmit = async () => {
+    try {
+      await this._validateInputs();
+      const { name, discloseDate, errors } = this.state;
+      const disclose_date = discloseDate.toString();
+
+      if (!Object.keys(errors).length) {
+        await this.props.dispatch(diaryActions.create({ name, disclose_date }));
+        const { navigation, diary } = this.props;
+        if (diary.fulfilledCreate) navigation.navigate("Home");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
-    const { name } = this.state;
+    const { name, discloseDate, errors } = this.state;
     return (
       <View style={styles.root}>
-        {/* <Text style={styles.titleText}>Create New Diary</Text>
+        <Text style={styles.titleText}>Create New Diary</Text>
         <View style={{ marginBottom: 16 }}>
           <TextField
             ref={this.nameRef}
@@ -136,50 +161,59 @@ class DiaryNew extends Component {
             onSubmitEditing={this._onSubmitDaiaryName}
             autoCapitalize="none"
             autoCorrect={false}
-            // error={errors.email}
+            error={errors.name}
           />
-        </View> */}
-        <MultiSelect
-          items={this.items}
-          uniqueKey="id"
-          ref={this.friendsRef}
-          onSelectedItemsChange={this._onSelectedItemsChange}
-          selectedItems={this.state.selectedItems}
-          selectText="Select friends to share"
-          searchInputPlaceholderText="Search Friends to share this diary"
-          onChangeInput={text => console.log(text)}
-          tagRemoveIconColor={COLOR.blue900}
-          tagBorderColor={COLOR.blue500}
-          tagTextColor={COLOR.blue500}
-          selectedItemTextColor={COLOR.blue500}
-          selectedItemIconColor={COLOR.blue500}
-          itemTextColor="#424242"
-          displayKey="name"
-          searchInputStyle={{ color: COLOR.blue500 }}
-          submitButtonColor={COLOR.blue500}
-          submitButtonText="Done"
-          style={{ backgroundColor: "red" }}
-          onPress={console.log("press")}
-        />
+        </View>
+        <View style={{ marginBottom: 16 }}>
+          <MultiSelect
+            items={this.items}
+            uniqueKey="id"
+            ref={this.friendsRef}
+            onSelectedItemsChange={this._onSelectedItemsChange}
+            selectedItems={this.state.selectedItems}
+            selectText="Select friends to share"
+            searchInputPlaceholderText="Search Friends to share this diary"
+            onChangeInput={text => console.log(text)}
+            tagRemoveIconColor={COLOR.blue900}
+            tagBorderColor={COLOR.blue500}
+            tagTextColor={COLOR.blue500}
+            selectedItemTextColor={COLOR.blue500}
+            selectedItemIconColor={COLOR.blue500}
+            itemTextColor="#424242"
+            displayKey="name"
+            searchInputStyle={{ color: COLOR.blue500 }}
+            submitButtonColor={COLOR.blue500}
+            submitButtonText="Done"
+            style={{ backgroundColor: "red" }}
+          />
+        </View>
         <View
           style={{
             paddingBottom: 8,
-            marginBottom: 16,
             borderBottomWidth: StyleSheet.hairlineWidth
           }}
         >
           <TouchableOpacity onPress={this._showDateTimePicker}>
             <View style={styles.button}>
               <Text>
-                {this.state.discloseDate instanceof Date
-                  ? `Disclose on ${format(
-                      this.state.discloseDate,
-                      "MMM Do, YYYY"
-                    )}`
+                {discloseDate
+                  ? `Disclose on ${format(discloseDate, "MMM Do, YYYY")}`
                   : "Select disclose date"}
               </Text>
             </View>
           </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginTop: 4,
+            marginBottom: 32
+          }}
+        >
+          {errors.discloseDate ? (
+            <Text style={{ color: COLOR.red700, fontSize: RF(1.8) }}>
+              {errors.discloseDate}
+            </Text>
+          ) : null}
         </View>
 
         <Button primary raised text="Create" onPress={this._onSubmit} />
