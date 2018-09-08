84871ce9f0f6cd999eac9c1edb74d9f6feb1f31a
Sunny Heo <mobileheo@gmail.com>
2018-09-07 18:45:49 -0700

    Change update action in DiariesController to respond with updated diary object

--- react_native_client/src/_helpers/_array.js
+++ react_native_client/src/_helpers/_array.js
@@ -1,7 +1,5 @@
 const ArrayExtends = {
   _replaceObj(newObj, key) {
-    console.log(newObj);
-    console.log(key);
     return this.map(obj => (obj[key] === newObj[key] ? newObj : obj));
   }
 };
