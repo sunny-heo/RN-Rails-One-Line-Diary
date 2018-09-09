ab78cd65970e8a03d2c4d17fbc6dadf59ec25847
Sunny Heo <mobileheo@gmail.com>
2018-09-09 03:55:19 -0700

    add full_name in User model

--- rails-backend/app/models/user.rb
+++ rails-backend/app/models/user.rb
@@ -19,4 +19,8 @@ class User < ApplicationRecord
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
     errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
