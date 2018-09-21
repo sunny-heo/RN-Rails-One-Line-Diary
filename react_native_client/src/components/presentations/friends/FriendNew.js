c087e9e627639f0077447455e981f37d70fb9419
Sunny Heo <mobileheo@gmail.com>
2018-09-20 17:35:55 -0700

    Add list of users from user search & add avatar on the left side of search item

--- react_native_client/src/components/presentations/friends/FriendNew.js
+++ react_native_client/src/components/presentations/friends/FriendNew.js
@@ -12,9 +12,11 @@ import {
   StyleSheet
 } from "react-native";
 
-import Icon from "react-native-vector-icons/MaterialIcons";
+import { Avatar, ListItem, ActionButton, Icon } from "react-native-material-ui";
 import { SearchBar } from "../../customSearchBar";
 import { searchActions } from "../../../actions";
+
+import { FlatList } from "react-native-gesture-handler";
 
 const mapStateToProps = (state, nextOwnProps) => state;
 const mapDispatchToProps = dispatch => {
@@ -29,6 +31,27 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
+const Row = ({ user, navigation }) => {
+  const { email, first_name, last_name } = user;
+  return (
+    <ListItem
+      divider
+      leftElement={
+        <Avatar
+          text={`${first_name[0].toUpperCase()}.${last_name[0].toUpperCase()}`}
+        />
+      }
+      centerElement={{
+        primaryText: `${first_name} ${last_name}`,
+        secondaryText: email
+      }}
+      // onPress={() => {
+      //   navigation.navigate("FriendProfile");
+      // }}
+    />
+  );
+};
+
 class FriendNew extends Component {
   constructor() {
     super();
@@ -36,17 +59,39 @@ class FriendNew extends Component {
   }
   onChangeText = text => {
     clearTimeout(this.timerId);
-    this.timerId = setTimeout(() => this.props.searchUser(text), 1000);
+    this.timerId = setTimeout(() => this.props.searchUser(text), 500);
   };
 
   render() {
+    const { userResult } = this.props.search;
     return (
       <View style={styles.root}>
         <SearchBar
           onChangeText={this.onChangeText}
           onPressCancel={() => console.log("pressed")}
         />
-        {/* <Text>{this.state.text}</Text> */}
+        <FlatList
+          // refreshControl={
+          //   <RefreshControl
+          //     refreshing={this.state.refreshing}
+          //     onRefresh={this._onRefresh}
+          //   />
+          // }
+          data={userResult}
+          renderItem={({ item: user }) => {
+            return (
+              // <IncomingReqSwipeable
+              //   incomingReq={incomingReq}
+              //   navigation={navigation}
+              //   confirmReq={confirmReq}
+              //   declineReq={declineReq}
+              // >
+              <Row user={user} />
+              // </IncomingReqSwipeable>
+            );
+          }}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
     );
   }
